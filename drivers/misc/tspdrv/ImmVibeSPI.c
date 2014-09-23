a8d72ab2e3ba6c3c4caf7a970e037cb185a36985
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-09-22 18:52:12 -0700

    tspdrv: Add a sysfs control for timed vibrator strength
    * tspdrv: Add LG's timed_output based interface
    * tspdrv: Make the timed state atomic
    
    Change-Id: I66dec312e52a3c8834a93f6b8970c94a0d6af923

--- drivers/misc/tspdrv/ImmVibeSPI.c
+++ drivers/misc/tspdrv/ImmVibeSPI.c
@@ -111,7 +111,7 @@ static void __iomem *virt_bases_v = NULL;
 #ifdef IMMVIBESPIAPI
 #undef IMMVIBESPIAPI
 #endif
-#define IMMVIBESPIAPI static
+#define IMMVIBESPIAPI 
 
 /*
 ** This SPI supports only one actuator.
@@ -339,8 +339,8 @@ IMMVIBESPIAPI VibeStatus ImmVibeSPI_ForceOut_AmpDisable(VibeUInt8 nActuatorIndex
 	        sm100_power_set(0, &vib);
 
 			if (atomic_read(&vib.gp1_clk_flag) == 1) {
-				clk_disable_unprepare(cam_gp1_clk);
 				atomic_set(&vib.gp1_clk_flag, 0);
+				clk_disable_unprepare(cam_gp1_clk);
 			}
 		} else {
 #ifdef CONFIG_TSPDRV_PMIC_VIBRATOR
@@ -355,6 +355,7 @@ IMMVIBESPIAPI VibeStatus ImmVibeSPI_ForceOut_AmpDisable(VibeUInt8 nActuatorIndex
 
     return VIBE_S_SUCCESS;
 }
+EXPORT_SYMBOL(ImmVibeSPI_ForceOut_AmpDisable);
 
 /*
 ** Called to enable amp (enable output force)
@@ -366,8 +367,8 @@ IMMVIBESPIAPI VibeStatus ImmVibeSPI_ForceOut_AmpEnable(VibeUInt8 nActuatorIndex,
     {
 		if(sm100_flag) {
 			if (atomic_read(&vib.gp1_clk_flag) == 0) {
-				clk_prepare_enable(cam_gp1_clk);
 				atomic_set(&vib.gp1_clk_flag, 1);
+				clk_prepare_enable(cam_gp1_clk);
 			}
 
 			sm100_power_set(1, &vib);
@@ -381,6 +382,7 @@ IMMVIBESPIAPI VibeStatus ImmVibeSPI_ForceOut_AmpEnable(VibeUInt8 nActuatorIndex,
 
     return VIBE_S_SUCCESS;
 }
+EXPORT_SYMBOL(ImmVibeSPI_ForceOut_AmpEnable);
 
 /*
 ** Called at initialization time to set PWM freq, disable amp, etc...
@@ -495,6 +497,14 @@ IMMVIBESPIAPI VibeStatus ImmVibeSPI_ForceOut_SetSamples(VibeUInt8 nActuatorIndex
 		}
     }
     return VIBE_S_SUCCESS;
+}
+
+/* For tuning of the timed interface strength */
+#define DEFAULT_TIMED_STRENGTH 65
+VibeInt8 timedForce = DEFAULT_TIMED_STRENGTH;
+
+VibeStatus ImmVibeSPI_SetTimedSample(void) {
+	return ImmVibeSPI_ForceOut_SetSamples(0, 8, 1, &timedForce);
 }
 
 /*
